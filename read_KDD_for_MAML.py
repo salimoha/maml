b60207cc20a189c5548f865205e346d05ec4da11
Shahrouz Ryan Alimo <salimoha@ucsd.edu>
2018-04-03 14:53:05 -0700

    reading the data

---
+++ read_KDD_for_MAML.py
@@ -0,0 +1,185 @@
+
+
+""" Code for loading data. """
+import numpy as np
+import os
+import random
+import tensorflow as tf
+
+from tensorflow.python.platform import flags
+from utils import get_images
+
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+
+FLAGS = flags.FLAGS
+
+class DataGenerator(object):
+    """
+    Data Generator capable of generating batches of sinusoid or Omniglot data.
+    A "class" is considered a class of omniglot digits or a particular sinusoid function.
+    """
+    def __init__(self, num_samples_per_class, batch_size, config={}):
+        """
+        Args:
+            num_samples_per_class: num samples to generate per class in one batch
+            batch_size: size of meta batch size (e.g. number of functions)
+        """
+        self.batch_size = batch_size
+        self.num_samples_per_class = num_samples_per_class
+        self.num_classes = 1  # by default 1 (only relevant for classification problems)
+
+
+        if FLAGS.datasource == 'KDD':
+            self.num_classes = config.get('num_classes', FLAGS.num_classes)
+            self.img_size = config.get('img_size', (316172,40))
+            self.dim_input = np.prod(self.img_size)
+            self.dim_output = self.num_classes
+            metatrain_folder = config.get('metatrain_folder', './data/KDD_data/train.csv')
+            if FLAGS.test_set:
+                metaval_folder = config.get('metaval_folder', './data/KDD_data/test.csv')
+            else:
+                metaval_folder = config.get('metaval_folder', './data/KDD_data/val.csv')
+
+            #metatrain_folders = []
+            #metaval_folders = [os.path.join(metaval_folder, label) \
+            #    for label in os.listdir(metaval_folder) \
+            #    if os.path.isdir(os.path.join(metaval_folder, label)) \
+            #    ]
+            #self.metatrain_character_folders = metatrain_folders
+            #self.metaval_character_folders = metaval_folders
+            self.rotations = config.get('rotations', [0])
+
+        elif 'omniglot' in FLAGS.datasource:
+            self.num_classes = config.get('num_classes', FLAGS.num_classes)
+            self.img_size = config.get('img_size', (28, 28))
+            self.dim_input = np.prod(self.img_size)
+            self.dim_output = self.num_classes
+            # data that is pre-resized using PIL with lanczos filter
+            data_folder = config.get('data_folder', './data/omniglot_resized')
+
+            character_folders = [os.path.join(data_folder, family, character) \
+                for family in os.listdir(data_folder) \
+                if os.path.isdir(os.path.join(data_folder, family)) \
+                for character in os.listdir(os.path.join(data_folder, family))]
+            random.seed(1)
+            random.shuffle(character_folders)
+            num_val = 100
+            num_train = config.get('num_train', 1200) - num_val
+            self.metatrain_character_folders = character_folders[:num_train]
+            if FLAGS.test_set:
+                self.metaval_character_folders = character_folders[num_train+num_val:]
+            else:
+                self.metaval_character_folders = character_folders[num_train:num_train+num_val]
+            self.rotations = config.get('rotations', [0, 90, 180, 270])
+
+
+        elif FLAGS.datasource == 'miniimagenet':
+            self.num_classes = config.get('num_classes', FLAGS.num_classes)
+            self.img_size = config.get('img_size', (84, 84))
+            self.dim_input = np.prod(self.img_size)*3
+            self.dim_output = self.num_classes
+            metatrain_folder = config.get('metatrain_folder', './data/miniImagenet/train')
+            if FLAGS.test_set:
+                metaval_folder = config.get('metaval_folder', './data/miniImagenet/test')
+            else:
+                metaval_folder = config.get('metaval_folder', './data/miniImagenet/val')
+
+            metatrain_folders = [os.path.join(metatrain_folder, label) \
+                for label in os.listdir(metatrain_folder) \
+                if os.path.isdir(os.path.join(metatrain_folder, label)) \
+                ]
+            metaval_folders = [os.path.join(metaval_folder, label) \
+                for label in os.listdir(metaval_folder) \
+                if os.path.isdir(os.path.join(metaval_folder, label)) \
+                ]
+            self.metatrain_character_folders = metatrain_folders
+            self.metaval_character_folders = metaval_folders
+            self.rotations = config.get('rotations', [0])
+        else:
+            raise ValueError('Unrecognized data source')
+
+
+    def make_data_tensor(self, train=True):
+        data = pd.read_csv("./data/KDD_data/train.csv")
+        data_x = data.loc[:, data.columns != 'col41']
+        data_x = data_x.apply(pd.to_numeric, errors='ignore')
+        labels = data.loc[:,data.columns == 'col41']
+        data_x = data_x.as_matrix()
+
+        
+        #data_x = tf.cast(data_x, tf.float64) 
+        labels = labels.as_matrix()
+        #print("data type:",type(data_x))
+        # make queue for tensorflow to read from
+        #filename_queue = tf.train.string_input_producer(tf.convert_to_tensor(data_x), shuffle=False)
+        print('Generating image processing ops')
+        #image_reader = tf.WholeFileReader()
+        #_, image_file = image_reader.read(filename_queue)
+        if FLAGS.datasource == 'miniimagenet':
+            image = tf.image.decode_jpeg(image_file, channels=3)
+            image.set_shape((self.img_size[0],self.img_size[1],3))
+            image = tf.reshape(image, [self.dim_input])
+            image = tf.cast(image, tf.float32) / 255.0
+        elif FLAGS.datasource == 'KDD':
+            data_x.set_shape((self.img_size[0],self.img_size[1],3))
+            image = tf.reshape(data_x, [self.dim_input])
+            image = tf.cast(image, tf.float32) 
+        else:
+            image = tf.image.decode_png(image_file)
+            image.set_shape((self.img_size[0],self.img_size[1],1))
+            image = tf.reshape(image, [self.dim_input])
+            image = tf.cast(image, tf.float32) / 255.0
+            image = 1.0 - image  # invert
+        num_preprocess_threads = 1 # TODO - enable this to be set to >1
+        min_queue_examples = 256
+        examples_per_batch = self.num_classes * self.num_samples_per_class
+        batch_image_size = self.batch_size  * examples_per_batch
+        print('Batching images')
+        images = tf.train.batch(
+                [image],
+                batch_size = batch_image_size,
+                num_threads=num_preprocess_threads,
+                capacity=min_queue_examples + 3 * batch_image_size,
+                )
+        all_image_batches, all_label_batches = [], []
+        print('Manipulating image data to be right shape')
+        for i in range(self.batch_size):
+            image_batch = images[i*examples_per_batch:(i+1)*examples_per_batch]
+
+            if FLAGS.datasource == 'omniglot':
+                # omniglot augments the dataset by rotating digits to create new classes
+                # get rotation per class (e.g. 0,1,2,0,0 if there are 5 classes)
+                rotations = tf.multinomial(tf.log([[1., 1.,1.,1.]]), self.num_classes)
+            label_batch = tf.convert_to_tensor(labels)
+            new_list, new_label_list = [], []
+            for k in range(self.num_samples_per_class):
+                class_idxs = tf.range(0, self.num_classes)
+                class_idxs = tf.random_shuffle(class_idxs)
+
+                true_idxs = class_idxs*self.num_samples_per_class + k
+                new_list.append(tf.gather(image_batch,true_idxs))
+                if FLAGS.datasource == 'omniglot': # and FLAGS.train:
+                    new_list[-1] = tf.stack([tf.reshape(tf.image.rot90(
+                        tf.reshape(new_list[-1][ind], [self.img_size[0],self.img_size[1],1]),
+                        k=tf.cast(rotations[0,class_idxs[ind]], tf.int32)), (self.dim_input,))
+                        for ind in range(self.num_classes)])
+                new_label_list.append(tf.gather(label_batch, true_idxs))
+            new_list = tf.concat(new_list, 0)  # has shape [self.num_classes*self.num_samples_per_class, self.dim_input]
+            new_label_list = tf.concat(new_label_list, 0)
+            all_image_batches.append(new_list)
+            all_label_batches.append(new_label_list)
+        all_image_batches = tf.stack(all_image_batches)
+        all_label_batches = tf.stack(all_label_batches)
+        all_label_batches = tf.one_hot(all_label_batches, self.num_classes)
+        return all_image_batches, all_label_batches
+
+
+
+
+
+
+
